3750bccbf0f0d7a87a3c9c14a4a28f05540c1a8a
Endika7593 <e.acedo93@gmail.com>
2023-08-29 11:48:39 +0200

    hechos los Posts demos

---
+++ src/Data/PostsData.js
@@ -0,0 +1,29 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.JPG'
+import postPic3 from '../img/postpic3.JPG'
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Tzuyu',
+        desc: "Urteberri on lagun guztioi! #2028",
+        likes: 2300,
+        liked: true 
+    },
+    {
+        img: postPic2,
+        name: 'Josécarlos',
+        desc: "Me voy a cagar en todo lo cagable.",
+        likes: 2300,
+        liked: false 
+    },
+    {
+        img: postPic3,
+        name: 'Chochito Volador',
+        desc: "Nosaes",
+        likes: 2300,
+        liked: true 
+    },
+    
+]
