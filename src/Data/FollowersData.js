0472a0d0b9329111e923ed6c54c229655759a7d3
Endika7593 <e.acedo93@gmail.com>
2023-08-29 12:13:20 +0200

    hechas las tendencias

--- src/Data/FollowersData.js
+++ src/Data/FollowersData.js
@@ -5,7 +5,7 @@ import img4 from '../img/img4.jpg';
 
 export const Followers = [
     { name: "Endika", username: "endika", img: img1 },
-    { namee: "Aitor", username: "malamute", img: img2 },
+    { name: "Aitor", username: "malamute", img: img2 },
     { name: "Ariane", username: "ariane", img: img3 },
     { name: "Jose", username: "jose", img: img4 },
 ]
---
+++ src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData= [
+    {
+        name: "Athletic",
+        shares: 99,
+    },
+    {
+        name: "Iñaki Williams",
+        shares: 98,
+    },
+    {
+        name: "Melon Musk",
+        shares: 97,
+    },
+    {
+        name: "ETA",
+        shares: 96,
+    },
+    {
+        name: "Anticristo",
+        shares: 95,
+    },
+]
