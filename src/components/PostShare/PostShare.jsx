d0b742dcd618a77c0a1aa5c9e57beac014a5262a
Endika7593 <e.acedo93@gmail.com>
2023-08-29 11:20:26 +0200

    post share hecho con el css, la X de cerrar y previsualizacion de imagen bastante makina

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import ProfileImage from '../../img/profileImg.jpg'
 import './PostShare.css'
 import { UilScenery } from '@iconscout/react-unicons';
 import { UilPlayCircle } from '@iconscout/react-unicons';
 import { UilLocationPoint } from '@iconscout/react-unicons';
 import { UilSchedule } from '@iconscout/react-unicons';
+import { UilTimes } from '@iconscout/react-unicons';
+
 const PostShare = () => {
+    const [image, setImage] = useState(null)
+    const imageRef = useRef()
+
+
+    const onImageChange =(event)=>{
+        if(event.target.files && event.target.files[0]) {
+            let img = event.target.files[0];
+            setImage({
+                image: URL.createObjectURL(img)
+            });
+        }
+    }
     return (
         <div className="PostShare">
             <img src={ProfileImage} alt="" />
@@ -15,6 +29,7 @@ const PostShare = () => {
                 <div className="postOptions">
                     <div className="option" 
                     style={{color: "var(--photo)"}}
+                    onClick={()=>imageRef.current.click()}
                     >
                         <UilScenery />
                         Foto
@@ -34,7 +49,26 @@ const PostShare = () => {
                         <UilSchedule />
                         Evento
                     </div>
+                    <button className="button ps-button">
+                        Compartir
+                    </button>
+                    <div style={{display: "none"}}>
+                        <input 
+                        type="file" 
+                        name="myImage" 
+                        ref={imageRef} 
+                        onChange={onImageChange} 
+                     />
+                    </div>
                 </div>
+                {image && (
+                    <div className="previewImage">
+                        <UilTimes onClick={()=>setImage(null)}/>
+                        <img src={image.image} alt="" />
+                    </div>
+                )}
+
+
             </div>
         </div>
     );
