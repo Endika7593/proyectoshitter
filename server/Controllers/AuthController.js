8a375b140c307dc8280b72e9ee011fa0783f6921
Endika7593 <e.acedo93@gmail.com>
2023-09-02 20:18:08 +0200

    react router dom hecho, vamos con la configuracion de compartir posts

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,6 +1,6 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 //registro usuario nuevo
 export const registerUser = async(req, res) => {
 
@@ -18,10 +18,14 @@ export const registerUser = async(req, res) => {
         {
             return res.status(400).json({message: "El nombre de usuario está cogido"})
         }
-        await newUser.save()
-        res.status(200).json(newUser)
+        const user = await newUser.save();
+
+        const token = jwt.sign({
+            username: user.username, id: user._id
+        }, process.env.JWT_KEY, {expiresIn: '1h'})
+        res.status(200).json({user, token});
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 };
 
@@ -40,7 +44,17 @@ export const loginUser = async (req, res) => {
 
 
 
-            validity? res.status(200).json(user): res.status(400).json("Contraseña incorrecta")
+            if(!validity)
+            {
+                res.status(400).json("Wrong password")
+            }
+            else
+            {
+                const token = jwt.sign({
+                    username: user.username, id: user._id
+                }, process.env.JWT_KEY, {expiresIn: '1h'})
+                res.status(200).json({user, token})
+            }
         }
         else{
             res.status(404).json("El usuario no existe")
